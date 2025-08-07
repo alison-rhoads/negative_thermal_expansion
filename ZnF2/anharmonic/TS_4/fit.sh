f2348482ca6b41b5b7c067169f06c49706828088
Alison Rhoads <121977692+alison-rhoads@users.noreply.github.com>
2025-08-07 10:54:13 -0700

    added scf3 and znf2 data

---
+++ ZnF2/anharmonic/TS_4/fit.sh
@@ -0,0 +1,11 @@
+pheasy -s --dim 3 3 4 -w 3 --c3 5 --nbody 2 3 
+echo "step 1 done"
+pheasy -s --read_scell --dim 3 3 4 -w 3 --c3 5 --nbody 2 3
+echo "step 2 done"
+pheasy -c --read_scell --dim 3 3 4 -w 3
+echo "step 3 done"
+pheasy -d --read_scell --dim 3 3 4 --ndata 4 --disp_file
+echo "step 4 done"
+pheasy -f --read_scell --dim 3 3 4 -w 3 --model LASSO --fix_fc2 --ndata 4 --hdf5 --full_ifc > fit.out
+echo "step 5 done"
+echo "FITTING COMPLETE!"
